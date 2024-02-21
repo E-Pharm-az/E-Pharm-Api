142e2801dc1e3ee9d469a78b740f7577dca01ea4
mzdevI <muhammed.mammadhuseynov@gmail.com>
2024-02-21 16:35:18 +0400

    Add infrastructure

--- PharmaPortalService/PharmaPortalService.Infrastructure/Interfaces/IRepository.cs
+++ PharmaPortalService/PharmaPortalService.Infrastructure/Interfaces/IRepository.cs
@@ -1,6 +1,12 @@
+using PharmaPortalService.Infrastructure.Context.Entities.Base;
+
 namespace PharmaPortalService.Infrastructure.Interfaces;
 
-public interface IRepository
+public interface IRepository<T> where T : BaseEntity
 {
-    
+    Task<IEnumerable<T>> GetAllAsync();
+    Task<T?> GetByIdAsync(int? id);
+    Task<T> InsertAsync(T entity);
+    Task<bool> UpdateAsync(T entity);
+    Task<bool> DeleteAsync(T entity);  
 }
