67a56c7d44235b7df57b80c4f358c9d0f1ec9e86
mzdevI <muhammed.mammadhuseynov@gmail.com>
2024-02-23 00:08:15 +0400

    Add minor chagnes to Repository Interface

--- PharmaPortalService/PharmaPortalService.Infrastructure/Interfaces/IRepository.cs
+++ PharmaPortalService/PharmaPortalService.Infrastructure/Interfaces/IRepository.cs
@@ -4,9 +4,10 @@ namespace PharmaPortalService.Infrastructure.Interfaces;
 
 public interface IRepository<T> where T : BaseEntity
 {
-    Task<IEnumerable<T>> GetAllAsync();
-    Task<T?> GetByIdAsync(int? id);
-    Task<T> InsertAsync(T entity);
-    Task<bool> UpdateAsync(T entity);
-    Task<bool> DeleteAsync(T entity);  
+    public Task<IEnumerable<T>> GetAllAsync();
+    public Task<T?> GetByIdAsync(int? id);
+    public Task InsertAsync(T entity);
+    void UpdateAsync(T entity);
+    void DeleteAsync(T entity);
+    public Task SaveChangesAsync();
 }
