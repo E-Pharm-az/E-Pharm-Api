454a6ecab10dbdd897b1870794dc8ab69893e887
mzdevI <muhammed.mammadhuseynov@gmail.com>
2024-02-25 23:15:37 +0400

    Add dtos

---
+++ PharmaPortalService/PharmaPortalService.Domain/Dtos/PharmaCompanyDtos/CreatePharmaCompanyDto.cs
@@ -0,0 +1,9 @@
+namespace PharmaPortalService.Domain.Dtos.PharmaCompanyDtos;
+
+public class CreatePharmaCompanyDto
+{
+    public string CompanyName { get; set; }
+    public string ContactEmail { get; set; }
+    public string ContactPhone { get; set; }
+    public string Location { get; set; }
+}
---
+++ PharmaPortalService/PharmaPortalService.Domain/Dtos/PharmaCompanyManagerDto/GetPharmaCompanyManagerDto.cs
@@ -0,0 +1,12 @@
+namespace PharmaPortalService.Domain.Dtos.PharmaCompanyManagerDto;
+
+public class GetPharmaCompanyManagerDto
+{
+    public int Id { get; set; }
+    public string ExternalId { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    public string Email { get; set; }
+    public string Phone { get; set; }
+    public int PharmaCompanyId { get; set; }
+}
