b8ec26256a56dba4548c9a53e9674bde9a534245
mzdevI <muhammed.mammadhuseynov@gmail.com>
2024-02-29 17:34:14 +0400

    Add dtos

---
+++ EPharm/EPharm.Domain/Dtos/UserDto/UserRegistrationDto.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace EPharm.Domain.Dtos.UserDto;
+
+public class UserRegistrationDto
+{
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    public string Email { get; set; }
+    [Required]
+    public string Fin { get; set; }
+    [Required]
+    public string PhoneNumber { get; set; }
+    [Required]
+    public string Password { get; set; }
+}
