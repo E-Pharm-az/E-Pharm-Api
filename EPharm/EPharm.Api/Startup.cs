57e88c0c79f4828b676a75e2a1c4e4f19a28f57f
mzdevI <muhammed.mammadhuseynov@gmail.com>
2024-02-29 17:32:56 +0400

    Add auth service registration

--- EPharm/EPharm.Api/Startup.cs
+++ EPharm/EPharm.Api/Startup.cs
@@ -1,11 +1,25 @@
-using EPharm.Domain.Interfaces;
-using EPharm.Domain.Services;
-using EPharm.Infrastructure.Interfaces;
+using System.Text;
+using EPharm.Domain.Interfaces.Jwt;
+using EPharm.Domain.Interfaces.Pharma;
+using EPharm.Domain.Interfaces.User;
+using EPharm.Domain.Services.Jwt;
+using EPharm.Domain.Services.Pharma;
+using EPharm.Domain.Services.User;
+using EPharm.Infrastructure.Context;
+using EPharm.Infrastructure.Context.Entities.Identity;
+using EPharm.Infrastructure.Interfaces.IdentityRepositoriesInterfaces;
 using EPharm.Infrastructure.Interfaces.JunctionsRepositoriesInterfaces;
+using EPharm.Infrastructure.Interfaces.PharmaRepositoriesInterfaces;
 using EPharm.Infrastructure.Interfaces.ProductRepositoriesInterfaces;
-using EPharm.Infrastructure.Repositories;
+using EPharm.Infrastructure.Repositories.IdentityRepositories;
 using EPharm.Infrastructure.Repositories.JunctionsRepositories;
+using EPharm.Infrastructure.Repositories.PharmaRepositories;
 using EPharm.Infrastructure.Repositories.ProductRepositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 
 namespace EPharmApi;
 
@@ -18,8 +32,81 @@ public class Startup(IConfiguration configuration)
         services.AddEndpointsApiExplorer();
         services.AddSwaggerGen();
         services.AddControllers();
-        
+
+        services.AddSwaggerGen(ops =>
+        {
+            ops.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme()
+            {
+                Name = "Authorization",
+                Type = SecuritySchemeType.ApiKey,
+                Scheme = "Bearer",
+                BearerFormat = "JWT",
+                In = ParameterLocation.Header,
+                Description =
+                    "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 1s\""
+            });
+
+            ops.AddSecurityRequirement(new OpenApiSecurityRequirement()
+            {
+                {
+                    new OpenApiSecurityScheme
+                    {
+                        Reference = new OpenApiReference
+                        {
+                            Type = ReferenceType.SecurityScheme,
+                            Id = "Bearer"
+                        }
+                    },
+                    Array.Empty<string>()
+                }
+            });
+        });
+
+        services.AddIdentity<AppIdentityUser, IdentityRole>(options =>
+            {
+                options.SignIn.RequireConfirmedAccount = false;
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequireDigit = true;
+                options.Password.RequiredLength = 8;
+                options.Password.RequireNonAlphanumeric = true;
+                options.Password.RequireUppercase = true;
+                options.Password.RequireLowercase = true;
+            })
+            .AddEntityFrameworkStores<AppIdentityDbContext>();
+
+        services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new()
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration["JwtSettings:Issuer"],
+                    ValidAudience = configuration["JwtSettings:Audience"],
+                    IssuerSigningKey =
+                        new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["JwtSettings:Key"]))
+                };
+            });
+
+        services.AddCors(ops =>
+            ops.AddPolicy("AllowAnyOrigins", builder => builder.AllowAnyOrigin()));
+
         services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+        services.AddDbContext<AppDbContext>(options =>
+            options.UseNpgsql(configuration.GetConnectionString("DefaultConnection")));
+
+        services.AddDbContext<AppIdentityDbContext>(
+            options => options.UseNpgsql(configuration.GetConnectionString("UserDefaultConnection")));
+
+        services.AddScoped<IAppIdentityUserRepository, AppIdentityUserRepository>();
 
         services.AddScoped<IPharmaCompanyRepository, PharmaCompanyRepository>();
         services.AddScoped<IPharmaCompanyManagerRepository, PharmaCompanyManagerRepository>();
@@ -27,7 +114,7 @@ public class Startup(IConfiguration configuration)
         services.AddScoped<IActiveIngredientRepository, ActiveIngredientRepository>();
         services.AddScoped<IAllergyRepository, AllergyRepository>();
         services.AddScoped<IDosageFormRepository, DosageFormRepository>();
-        services.AddScoped<IIndicationRepository, IIndicationRepository>();
+        services.AddScoped<IIndicationRepository, IndicationRepository>();
         services.AddScoped<IManufacturerRepository, ManufacturerRepository>();
         services.AddScoped<IProductImageRepository, ProductImageRepository>();
         services.AddScoped<IRegulatoryInformationRepository, RegulatoryInformationRepository>();
@@ -46,6 +133,11 @@ public class Startup(IConfiguration configuration)
 
         services.AddScoped<IPharmaCompanyService, PharmaCompanyService>();
         services.AddScoped<IPharmaCompanyManagerService, PharmaCompanyManagerService>();
+        services.AddScoped<IUserService, UserService>();
+
+        services.AddScoped<ITokenService, TokenService>();
+        services.AddScoped<ITokenCreationService, TokenCreationService>();
+        services.AddScoped<ITokenRefreshService, TokenRefreshService>();
     }
 
     public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -55,5 +147,15 @@ public class Startup(IConfiguration configuration)
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+
+        app.UseCors("AllowAnyOrigins");
+
+        app.UseHttpsRedirection();
+        app.UseRouting();
+        app.UseAuthentication();
+        app.UseAuthorization();
+        app.UseHsts();
+        app.UseEndpoints(endpoints => { endpoints.MapControllers(); });
+        
     }
 }
