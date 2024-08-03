7698f077e4e2d4d1b9226dfed7dc5a607fb5a6eb
mzdevI <muhammed.mammadhuseynov@gmail.com>
2024-08-03 18:23:28 +0400

    Sync up

--- EPharm/EPharm.Domain/Dtos/PharmacyDtos/GetPharmacyDto.cs
+++ EPharm/EPharm.Domain/Dtos/PharmacyDtos/GetPharmacyDto.cs
@@ -8,6 +8,7 @@ public class GetPharmacyDto
     public GetUserDto Owner { get; set; }
     public string TIN { get; set; }
     public string Name { get; set; }
+    public string OwnerEmail { get; set; }
     public string Email { get; set; }
     public string Phone { get; set; }
     public string Website { get; set; }
